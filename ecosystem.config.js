ffc1d67205de98d6a82b82f5ab5ff4020a4df544
Jemisson <jemison321@gmail.com>
2023-06-23 11:29:05 -0400

    alteração de staging 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: 'procstudio',
-      script: 'pnpm',
-      args: 'start',
-      cwd: '/home/staging/procstudio',
-      env: {
-        NODE_ENV: 'staging',
-      },
-    },
-  ],
+  apps: [{
+    script: 'npm start'
+  }],
+
+  deploy: {
+    production: {
+      // key: 'key.pem',
+      user: 'staging',
+      host: '34.214.28.153',
+      ref: 'origin/developer',
+      repo: 'git@github.com:Jemisson/procstudio.git',
+      path: '/home/staging',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
 };
